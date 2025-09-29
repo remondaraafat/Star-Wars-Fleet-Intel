556a093de00d7694fc77aea265bd2d624a0cd7f2
remondaraafat <remondarafat2001@gmail.com>
2025-09-29 21:47:24 +0300

    feat(backend:API): update appsettings.json for logging and correlation

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Star-Wars-Fleet-Intel" 
---
+++ backend/Application/Interfaces/ISwapiFacadeService.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface ISwapiFacadeService
+    {
+        Task<IEnumerable<Starship>> GetStarshipsAsync(string? search = null, CancellationToken ct = default);
+    }
+}
